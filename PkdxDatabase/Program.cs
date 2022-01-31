1ca344335bfd5b10a4ea43d238c128aa733ee84e
Joshua Baldwin <jbald9312@gmail.com>
2022-01-31 09:45:31 -0500

    dont move window

--- PkdxDatabase/Program.cs
+++ PkdxDatabase/Program.cs
@@ -6,23 +6,8 @@ namespace Pokedex.PkdxDatabase
 {
     class Program
     {
-        const int SWP_NOSIZE = 0x0001;
-
-
-        [DllImport("kernel32.dll", ExactSpelling = true)]
-        private static extern IntPtr GetConsoleWindow();
-
-        private static IntPtr MyConsole = GetConsoleWindow();
-
-        [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
-        public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
-
         static void Main(string[] args)
         {
-            int xpos = -1300;
-            int ypos = 300;
-            SetWindowPos(MyConsole, 0, xpos, ypos, 0, 0, SWP_NOSIZE);
-
             using var context = PokedexDbContextFactory.Instance.CreateDbContext();
             PopulateDB.PopulateDatabase(context);
         }
